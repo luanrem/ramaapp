ee520f9a573180b50c4896608b47b2b780c915f6
luanrem <luanrem@gmail.com>
2021-06-09 21:27:44 -0300

    index header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,104 @@
-import { Container } from '../styles/pages/Home'
+import React, { useCallback, useState } from 'react'
+
+import BackToTopIcon from '../components/BackToTopIcon/BackToTopIcon'
+
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded'
+import {
+  Button,
+  ClickAwayListener,
+  ListItem,
+  ListItemText,
+  Paper,
+  List,
+  Hidden
+} from '@material-ui/core'
+import Poppers from '@material-ui/core/Popper'
+import Link from 'next/link'
+
+import { Container, Header, MotionHeaderMenu } from '../styles/pages/Home'
+import { useWindowScroll } from 'react-use'
 
 export default function Home() {
+  const [openMenu, setOpenMenu] = useState<null | HTMLElement>(null)
+  const { y } = useWindowScroll()
+
+  const handleClickMenu = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      setOpenMenu(openMenu ? null : event.currentTarget)
+    },
+    [openMenu]
+  )
+
+  const handleCloseMenu = useCallback(() => {
+    setOpenMenu(null)
+  }, [setOpenMenu])
+
   return (
     <Container>
-      <h1>Hello World</h1>
+      <Header headerBackground={y > 80}>
+        <div className="space" />
+
+        <Hidden mdDown>
+          <ul>
+            <li>
+              <a href="#QuemSomosNosC">Quem somos nós</a>
+            </li>
+            <li>
+              <a href="#NossaMeta">Nossa Meta</a>
+            </li>
+            <li>
+              <a href="#ComoParticipar">Como Participar</a>
+            </li>
+            <li>
+              <a href="#GaleriaDeAtividades">Galeria de atividades</a>
+            </li>
+            <li>
+              <a href="#Contato">Contato</a>
+            </li>
+          </ul>
+        </Hidden>
+
+        <Button className="VertIcon" onClick={handleClickMenu}>
+          <MenuRoundedIcon fontSize="large" />
+        </Button>
+
+        <Poppers open={Boolean(openMenu)} anchorEl={openMenu}>
+          <MotionHeaderMenu
+            initial={{
+              scaleY: 0,
+              y: -100
+            }}
+            animate={{
+              scaleY: 1,
+              y: 0
+            }}
+            transition={{
+              type: 'tween'
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleCloseMenu}>
+                <List>
+                  <Link href="/auth/signin">
+                    <ListItem button onClick={handleCloseMenu}>
+                      <ListItemText primary="Login" />
+                    </ListItem>
+                  </Link>
+                  <Link href="/auth/signup">
+                    <ListItem button onClick={handleCloseMenu}>
+                      <ListItemText primary="Criar Conta" />
+                    </ListItem>
+                  </Link>
+                </List>
+              </ClickAwayListener>
+            </Paper>
+          </MotionHeaderMenu>
+        </Poppers>
+      </Header>
+
+      <div className="vertialSpace"></div>
+
+      <BackToTopIcon showBelow={80} />
     </Container>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,5 +1,73 @@
 import styled from 'styled-components'
 
+import { motion } from 'framer-motion'
+
+interface HeaderProps {
+  headerBackground: boolean
+}
+
 export const Container = styled.div`
-  color: #8257e5;
+  background: var(--blue-twitter);
+
+  .vertialSpace {
+    height: 2100px;
+  }
 `
+export const Header = styled.header<HeaderProps>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 50px;
+  max-width: 2000px;
+  z-index: 1;
+
+  margin: 0 auto;
+  transition: 0.5s;
+  background: ${props =>
+    props.headerBackground === true ? 'white' : 'transparent'};
+  box-shadow: ${props =>
+    props.headerBackground === true ? '0 1px 3px black' : 'none'};
+
+  ul {
+    display: flex;
+
+    list-style: none;
+    font-size: 18px;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
+    transition: 0.3s;
+    color: ${props => (props.headerBackground === true ? 'black' : 'white')};
+
+    li {
+      display: inline-block;
+      padding: 0px 20px;
+      cursor: pointer;
+    }
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    li a::after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 2px;
+      background: black;
+      background: ${props =>
+        props.headerBackground === true ? 'black' : 'white'};
+      transition: width 0.4s;
+    }
+
+    li a:hover::after {
+      width: 100%;
+      transition: width 0.4s;
+    }
+  }
+`
+export const MotionHeaderMenu = styled(motion.div)``
